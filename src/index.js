61fd4745e0d96172a5d00e31636aaf51d8421fb2
davidgreenspan15 <ramigreenspan@gmail.com>
2019-12-01 03:47:07 -0500

    added payload and created seperate reducer file and imported into index.js

--- src/index.js
+++ src/index.js
@@ -3,44 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducer.js'
+
 
 import {createStore} from 'redux'
 
-const defaultState ={
-  likes: 0,
-  text: "",
-  darkMode: false,
-  thangs: []
-}
 
-function reducer (prevState=defaultState, action){
-  switch(action.type){
-    case "LIKE":
-      return {...prevState, likes: prevState.likes + 1}
-    case "DISLIKE":
-      return {...prevState, likes: prevState.likes - 1}
-    case "DARK_MODE":
-      return {...prevState, darkMode: !prevState.darkMode}
-    case "HANDLE_CHANGE":
-      return {...prevState, text: action.payload}
-    case "PRINT":
-      return {...prevState, thangs: [...prevState.thangs, prevState.text], text: ""}
-    case "DEPRINT":
-      return {
-        ...prevState,
-        thangs: prevState.thangs.filter(thang => {
-          return thang !== action.payload
-        })}
-    default:
-      return prevState
-  }
-
-}
 
 
 const store = createStore(reducer)
 
-store.dispatch({Type: "LIKE"})
+store.dispatch({type: "HANDLE_CHANGE", payload: "Hello World!"})
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
