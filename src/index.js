26191765cde42c99ed83d3bf8c37394ccfc3fc11
davidgreenspan15 <ramigreenspan@gmail.com>
2019-12-01 03:37:50 -0500

    added remaining reducer switch statements

--- src/index.js
+++ src/index.js
@@ -17,6 +17,24 @@ function reducer (prevState=defaultState, action){
   switch(action.type){
     case "LIKE":
       return {...prevState, likes: prevState.likes + 1}
+    case "DISLIKE":
+      return {...prevState, likes: prevState.likes - 1}
+    case "DARK_MODE":
+      return {...prevState, darkMode: !prevState.darkMode}
+    case "HANDLE_CHANGE":
+      return {...prevState, text: action.payload}
+    case "PRINT":
+      return {...prevState, thangs: [...prevState.thangs, prevState.text], text: ""}
+    case "DEPRINT":
+      return {
+        ...prevState,
+        thangs: prevState.thangs.filter(thang => {
+          return thang !== action.payload
+        })}
+    default:
+      return prevState
+  }
+
 }
 
 
